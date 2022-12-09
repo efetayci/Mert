d5670a263f48fd6bd3a3e98187d0b498cf88db9e
Efe Taycı <efefehmitayci@gmail.com>
2022-12-09 19:00:26 +0300

    first commit efe

---
+++ 3.1MertSharp/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _3._1MertSharp
+{
+
+
+
+    public class Baseclass
+    {
+        public int a;
+        private int b;
+        protected int c;
+
+        public void fun()
+        {
+            Console.Write("Base class" + " " + c);
+        }
+    }
+    public class Derived1 : Baseclass
+    {
+        public void fun2()
+        {
+            Console.WriteLine(base.c);
+        }
+    }
+
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Derived1 d = new Derived1();
+            d.fun();
+            Console.WriteLine(d.c);
+
+            
+        }
+    }
+}
+
+    
+
